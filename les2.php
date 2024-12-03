5d096964fa305e21ec2d0ffd93e2db0fe689a624
Suhailism49 <9022614@student.zadkine.nl>
2024-12-03 13:59:01 +0100

    Add files via upload
    les2.php v1

---
+++ les2.php
@@ -0,0 +1,15 @@
+<?php
+//  Een voorbeeld van een echo en een print 
+echo "<h1>Hallo wereld,</h1>"; 
+echo "Ik zit op het "; 
+
+// Voorbeeld van een variabele in PHP. 
+$schoolnaam = "Techniek College Rotterdam !"; 
+
+// Met echo kun je iets op het scherm tonen. 
+echo "$schoolnaam";
+
+// Eigen commentaar-regel
+$leeftijd = 18;
+echo "<p>Ik ben $leeftijd jaar oud</p>";
+?>
