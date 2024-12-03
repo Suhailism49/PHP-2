39354ea9bb3cb2ba355f9d61ee42c93ad968353a
Suhailism49 <9022614@student.zadkine.nl>
2024-12-03 14:03:39 +0100

    Add files via upload
    les3.php v1

---
+++ les3.php
@@ -0,0 +1,15 @@
+<?php
+// String variabelen met echo
+$voornaam = "Suhail";
+$achternaam = "Ismaili";
+
+echo "Mijn voornaam is: $voornaam <br>";
+echo "Mijn achternaam is: $achternaam <br>";
+
+// Numerieke variabelen met print
+$leeftijd = 18;
+$jaar = 2024;
+
+print "Mijn leeftijd is: $leeftijd <br>";
+print "Huidige jaar is: $jaar <br>";
+?>
